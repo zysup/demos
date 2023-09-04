a0d6d9b693285fd19c35b7fe2ab6842f76bc944d
ck <ck@ck.com>
2023-09-04 15:18:51 +0800

    asd

--- js/rem.js
+++ js/rem.js
@@ -1,23 +1,23 @@
 (function (doc, win) {
   var docEl = doc.documentElement,
-      // resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-      recalc = function () {
-          var clientWidth = docEl.clientWidth;
-          if (!clientWidth) return;
-          console.log(clientWidth);
-          if (clientWidth > 414) {
-              docEl.style.fontSize = '100px';
+    // resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      console.log(clientWidth);
+      if (clientWidth > 414) {
+        docEl.style.fontSize = '100px';
 
-          } else {
-              docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
-          }
-      };
+      } else {
+        docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+      }
+    };
   if (!doc.addEventListener) return;
   // win.addEventListener(resizeEvt, recalc, false);
   win.addEventListener('resize', recalc, false);
   if (doc.readyState === 'loading') {  // 此时加载尚未完成
-      doc.addEventListener('DOMContentLoaded', recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
   } else {  // 此时`DOMContentLoaded` 已经被触发
-      recalc();
+    recalc();
   }
 })(document, window);
