25df694623f3af5478ae89dc1698053efb494d0b
ck <ck@ck.com>
2023-09-04 10:08:15 +0800

    asd

---
+++ jsonp/index.js
@@ -0,0 +1,24 @@
+function stringify (data) {
+  const pairs = Object.entries(data)
+  const qs = pairs.map(([k, v]) => {
+    let noValue = false
+    if (v === null || v === undefined || typeof v === 'object') {
+      noValue = true
+    }
+    return `${encodeURIComponent(k)}=${noValue ? '' : encodeURIComponent(v)}`
+  }).join('&')
+  return qs
+}
+
+function jsonp ({ url, onData, params }) {
+  const script = document.createElement('script')
+
+  // 一、为了避免全局污染，使用一个随机函数名
+  const cbFnName = `JSONP_PADDING_${Math.random().toString().slice(2)}`
+  // 二、默认 callback 函数为 cbFnName
+  script.src = `${url}?${stringify({ callback: cbFnName, ...params })}`
+  // 三、使用 onData 作为 cbFnName 回调函数，接收数据
+  window[cbFnName] = onData;
+
+  document.body.appendChild(script)
+}
